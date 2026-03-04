96c496c44b11a829cf52cb7eda3ff3d713bdf650
ggoswami777 <gaurav.goswami1304@gmail.com>
2026-03-04 22:00:01 +0530

    added the background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Home = () => {
+const page = () => {
   return (
-    <div>Home</div>
+    <section>
+        <h1 className='text-center'>The Hub for Every Dev <br /> Event You Mustn't Miss</h1>
+        <p className='text-center mt-5 '>Hackathons,Meetups and Conferences , All in One Place</p>
+    </section>
+    
   )
 }
 
-export default Home
+export default page
